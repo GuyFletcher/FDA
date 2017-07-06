731f74a18ed1023692f6a47d9f33cd292cb84212
Fletcher Hart <fletcher.hart@mymail.champlain.edu>
2017-07-06 12:19:57 -0400

    Get JSON
    Gets JSON form FDA URL

--- FDAApp/app/src/main/java/com/fletcherhart/fdaapp/MainFragment.java
+++ FDAApp/app/src/main/java/com/fletcherhart/fdaapp/MainFragment.java
@@ -1,8 +1,11 @@
 package com.fletcherhart.fdaapp;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +18,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import android.content.Context;
 
 /**
  * Created by Fletcher on 6/20/2017.
@@ -28,6 +34,8 @@ public class MainFragment extends Fragment {
     private EditText mSearch;
     private TextView mText;
     private static String urlFDA = "https://api.fda.gov/drug/event.json?limit=1";
+    public ProgressDialog pd;
+    Context context;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
@@ -40,13 +48,8 @@ public class MainFragment extends Fragment {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    try {
-                        mText.setText(fetchData(mSearch.getText().toString()));
-                    }
-                    catch(IOException ex)
-                    {
-                        System.out.println(ex.getMessage());
-                    }
+
+                    new JsonTask().execute("https://api.fda.gov/drug/event.json?limit=1");
 
                     //submit_btn.performClick();
                     return true;
@@ -66,7 +69,7 @@ public class MainFragment extends Fragment {
         String[] data = search.split(" ");
 
         URL url = new URL(urlFDA);
-        URLConnection conn = url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         conn.setRequestProperty("User-Agent", "Mozilla/5.0");
         InputStream is = conn.getInputStream();
 
@@ -80,5 +83,73 @@ public class MainFragment extends Fragment {
 
 
         return sb.toString();
+    }
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(getActivity());
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            mText.setText(result);
+        }
     }
 }
